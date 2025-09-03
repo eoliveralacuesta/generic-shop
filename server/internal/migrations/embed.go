78d5b7b24084460b63620814cac7da2bc98fc784
Evelyn <evelyn.olivera@zureo.com>
2025-09-03 16:45:02 -0300

    Update embed.go

--- server/internal/migrations/embed.go
+++ server/internal/migrations/embed.go
@@ -4,3 +4,5 @@ import "embed"
 
 //go:embed db/*.sql
 var Files embed.FS
+
+//Esto empaqueta los archivos .sql en un FS virtual para poder acceder a ellos desde cmd/migrate y ejecutar las actualizaciones de BD
